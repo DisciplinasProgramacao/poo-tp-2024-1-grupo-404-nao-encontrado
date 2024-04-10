9aed5a1002c27d4430b10c4688eafac1257a922e
lucascsantospuc <136100926+lucascsantospuc@users.noreply.github.com>
2024-04-10 10:21:42 -0300

    Add files via upload

---
+++ codigo/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Cliente
+{
+    private string _nome;
+
+    public Cliente(string nome)
+    {
+        this._nome = nome;
+    }
+
+    public string GetNome()
+    {
+        return _nome;
+    }
+}
