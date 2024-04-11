fb19df0ea9cb303fa0d903072c997314be39e5b0
diegao56 <diego56bola@hotmail.com>
2024-04-11 11:59:48 -0300

    Adicionado classe Mesa

---
+++ codigo/Mesa.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurante
+{
+    internal class Mesa
+    {
+        private int _capacidade;
+        private bool _estaOcupada;
+
+        public Mesa(int capacidade)
+        {
+            _capacidade = capacidade;
+            _estaOcupada = false;
+        }
+
+        public int GetCapacidade()
+        {
+            return _capacidade;
+        }
+
+        public bool GetEstaOcupada()
+        {
+            return _estaOcupada;
+        }
+
+        public void SetEstaOcupada(bool valor)
+        {
+            _estaOcupada = valor;
+        }
+
+        public string Relatorio()
+        {
+            return $"Mesa com capacidade para {_capacidade} pessoas." +
+                   $" Está ocupada: {_estaOcupada}.";
+        }
+
+        public bool VerificarDisponibilidade(int qntPessoas)
+        {
+            // Retorna verdadeiro se a mesa não está ocupada e se a quantidade de pessoas não excede a capacidade
+            return !_estaOcupada && qntPessoas <= _capacidade;
+        }
+    }
+}
