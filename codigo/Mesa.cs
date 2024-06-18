68c5ca7130b05eb83a9238e1e5159057e24d2c50
Lucas Bryan S. Quintas <102496302+BryanQuintas@users.noreply.github.com>
2024-06-17 23:25:55 -0300

    Update Mesa.cs - classe Mesa de "public" a "internal"

--- codigo/Mesa.cs
+++ codigo/Mesa.cs
@@ -1,52 +1,49 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Restaurante
+public class Mesa
 {
-    internal class Mesa
+    private int _capacidade;
+    private bool _estaOcupada;
+
+    public Mesa(int capacidade)
     {
-        private int _capacidade;
-        private bool _estaOcupada;
+        _capacidade = capacidade;
+        _estaOcupada = false;
+    }
 
-        public Mesa(int capacidade)
-        {
-            _capacidade = capacidade;
-            _estaOcupada = false;
-        }
+    public int GetCapacidade()
+    {
+        return _capacidade;
+    }
 
-        public int GetCapacidade()
-        {
-            return _capacidade;
-        }
+    public bool GetEstaOcupada()
+    {
+        return _estaOcupada;
+    }
 
-        public bool GetEstaOcupada()
-        {
-            return _estaOcupada;
-        }
+    public void SetEstaOcupada(bool valor)
+    {
+        _estaOcupada = valor;
+    }
 
-        public void SetEstaOcupada(bool valor)
-        {
-            _estaOcupada = valor;
-        }
+    public string Relatorio()
+    {
+        return $"Mesa com capacidade para {_capacidade} pessoas." +
+               $" Está ocupada: {_estaOcupada}.";
+    }
 
-        public string Relatorio()
-        {
-            return $"Mesa com capacidade para {_capacidade} pessoas." +
-                   $" Está ocupada: {_estaOcupada}.";
-        }
+    public bool VerificarDisponibilidade(int qntPessoas)
+    {
+        // Retorna verdadeiro se a mesa não está ocupada e se a quantidade de pessoas não excede a capacidade
+        return !_estaOcupada && qntPessoas <= _capacidade;
+    }
 
-        public bool VerificarDisponibilidade(int qntPessoas)
-        {
-            // Retorna verdadeiro se a mesa não está ocupada e se a quantidade de pessoas não excede a capacidade
-            return !_estaOcupada && qntPessoas <= _capacidade;
-        }
-
-        public string DescricaoOcupacao()
-        {
-            return _estaOcupada ? "Ocupada" : "vazia";
-        }
+    public string DescricaoOcupacao()
+    {
+        return _estaOcupada ? "Ocupada" : "vazia";
     }
 }
